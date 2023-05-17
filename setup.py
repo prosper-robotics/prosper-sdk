c8f6c19ce7d2f35758b366ff72793824ed6a12ea
jajoosam <sam@jajoo.fun>
2023-05-17 15:07:46 +0100

    package

---
+++ setup.py
@@ -0,0 +1,13 @@
+    from distutils.core import setup
+    setup(
+        name = 'prosper',
+        packages = ['prosper'],
+        version = '0.1',
+        description = 'Where the prosper SDK will live',
+        author = 'Prosper Robotics',
+        author_email = 'samarth@prosper.org',
+        url = 'https://github.com/prosper-robotics/prosper-sdk',
+        download_url = 'https://github.com/prosper-robotics/prosper-sdk/tarball/0.1',
+        keywords = ["robot"], # arbitrary keywords
+        classifiers = [],
+    )
